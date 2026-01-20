6a5aae64ba154858abc0234915a7f3679c755edb
DeepakYadav9892 <deepak989241@gmail.com>
2026-01-20 15:22:04 +0530

    second_commit_of_20_jan

---
+++ 11_Exception_handling/11.7_file_handling.py
@@ -0,0 +1,12 @@
+# file=open("order.txt","w")
+# file.write("Masala Chai -2 cups ")
+
+# try:
+#     file.write("Masala chai -2 cups")
+
+# finally:
+#     file.close()
+
+with open("order.txt","w") as file:
+    file.write("ginger tea- 4 cups")
+    
